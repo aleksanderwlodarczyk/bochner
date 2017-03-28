3e51138c57ed12c1dd7e5d61b90b08e3265fa85a
Arsemi337 <arturmilosz@onet.pl>
2017-03-28 18:27:35 +0200

    Poprawiony prędkościomierz
    Prędkościomierz skaluje się razem z ekranem, wskazówka obraca się teraz
    również przy cofaniu i maksymalną wartosć prędkościomierz osiąga
    dopiero, jak gracz złapie masło.

--- Mad Bread/Assets/Skrypty/SpeedometerGracz2.cs
+++ Mad Bread/Assets/Skrypty/SpeedometerGracz2.cs
@@ -6,6 +6,7 @@ public class SpeedometerGracz2 : MonoBehaviour {
 
 	static float minAngle = 32.0f;
 	static float maxAngle = -209.0f;
+	static float maxAngleBack = -28.0f;
 	static SpeedometerGracz2 thisSpeedo1;
 
 	// Use this for initialization
@@ -17,7 +18,12 @@ public class SpeedometerGracz2 : MonoBehaviour {
 	public static void ShowSpeed (float speed, float min, float max)
 	{
 		float ang = Mathf.Lerp (minAngle, maxAngle, Mathf.InverseLerp (min, max+15, speed));
-		thisSpeedo1.transform.eulerAngles = new Vector3 (0, 0, ang);
+		float ang2 = Mathf.Lerp (minAngle, maxAngleBack, Mathf.InverseLerp (min-10, max-30, speed));
+		if (speed >= 0.0f) {
+			thisSpeedo1.transform.eulerAngles = new Vector3 (0, 0, ang);
+		} else {
+			thisSpeedo1.transform.eulerAngles = new Vector3 (0, 0, -ang2);
+		}
 	}
 
 }

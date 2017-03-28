7ba01d60fb0c3ad1189383eb1abceecd1b23763d
Anataro <zambazon4@gmail.com>
2017-03-28 15:15:50 +0200

    Sementy ruchome oraz Pick Upy z Bąblem

---
+++ Mad Bread/Assets/Skrypty/Krecenie.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Krecenie : MonoBehaviour {
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (15, 30, 0.0f) * Time.deltaTime);
+	}
+}
---
+++ Mad Bread/Assets/Skrypty/Straznik.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Straznik : MonoBehaviour {
+
+	public Waypoint[] path;
+	public float reachDist = 0.1f;
+	public int currentWaypoint; 
+	public bool pause;
+	public float speed; 
+
+
+	Transform player; 
+
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+
+
+		
+		Patrol ();
+
+	}
+
+
+	void Patrol ()
+	{
+		float distance = Vector3.Distance (path [currentWaypoint].transform.position, transform.position);
+
+		if (!pause) {
+			transform.position = Vector3.MoveTowards (transform.position, path [currentWaypoint].transform.position, speed * Time.deltaTime);
+
+
+
+			if (distance < reachDist) {
+				
+				StartCoroutine (GuardPause ());
+				currentWaypoint++;
+
+
+
+			
+				if (currentWaypoint >= path.Length) {
+
+
+					currentWaypoint = 0;
+				}
+			}	
+		}
+}
+
+	void DetectPlayer()
+	{
+		
+	}
+
+
+
+
+
+	IEnumerator GuardPause()
+	{
+		pause = true;
+		yield return new WaitForSeconds (path [currentWaypoint].pauseTime);
+		pause = false;
+	}
+}

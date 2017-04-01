49081fba0ba7b0c38c5fcd2e51383011abc2ba80
vburtoch <vburtoch@gmail.com>
2017-04-01 22:14:56 +0200

    responsywność paska postępu
    pasek niezależnie od wybranej rozdzielczości działa tak samo

--- Mad Bread/Assets/Skrypty/PozycjaG1.cs
+++ Mad Bread/Assets/Skrypty/PozycjaG1.cs
@@ -2,10 +2,15 @@
 
 public class PozycjaG1 : MonoBehaviour
 {
-    private float stara, dystans, RóżnicaIloraz, różnica, DystansStart,pochodna;
+    private float stara, dystans, RóżnicaIloraz, różnica, DystansStart, pochodna, ratio, k;
     private int i;
+    private float x, y, P1, P2; //rozdzielczość
     void Start()
     {
+        P1 = Screen.height * Screen.width; // rozdzielczość ekranu
+        P2 = 1920 * 1080; //domyślna rozdzielczość
+        k = Mathf.Sqrt(P1 / P2);
+        ratio = k * 1000f;
         i = 1;
     }
 
@@ -14,7 +19,7 @@ public class PozycjaG1 : MonoBehaviour
 		if(i==1) DystansStart = Vector3.Distance(GameObject.FindGameObjectWithTag("Gracz1").transform.position, GameObject.FindGameObjectWithTag("Finish").transform.position);
         i = 2;
 		dystans = Vector3.Distance(GameObject.FindGameObjectWithTag("Gracz1").transform.position, GameObject.FindGameObjectWithTag("Finish").transform.position);
-        różnica = 1000 * (1 - dystans / DystansStart);
+        różnica = ratio* (1 - dystans / DystansStart);
         pochodna = różnica - stara;
         stara = różnica;
         transform.Translate(pochodna, 0, 0);
